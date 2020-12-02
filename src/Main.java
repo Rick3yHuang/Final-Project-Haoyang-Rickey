887a930b0b500eda8ffbc6603070396087f984f4
rickeyhuang <huanr19@wfu.edu>
2020-12-03 00:04:49 +0800

    Initial.

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/*
+Haoyang Du & Ruiqi (Rickey) Huang
+CS112 - Fall 2020
+Section C
+Blockchain Final Project
+ */
+
+package src;
+
+public class Main {
+
+    public static void main(String [] args){
+
+
+
+    }
+
+}
